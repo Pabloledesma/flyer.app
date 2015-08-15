2ee384f2922eeb1d51c63de656b2d4be1014bae3
Pablo Ledesma <pabloledes83@gmail.com>
2015-08-15 12:05:34 -0500

    Initial Routing

---
+++ resources/views/flyers/create.blade.php
@@ -0,0 +1,7 @@
+@extends('app')
+
+@section('content')
+
+	<h1>Selling your home?</h1>
+
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+
+@section('content')
+
+	<div class="jumbotron"> 
+		<h1>Flyer Application</h1>
+
+		<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. 
+		Use it as a starting point to create something more unique.</p>
+
+		<a href="/flyers/create" class="btn btn-primary">Create Flyer</a>
+	</div>	
+
+@stop
